5c1338dcfa0a83390312a329a295a6d518c7072a
jason <jimej14@farmingdale.edu>
2024-09-13 11:24:48 -0400

    Comments, and changes to the formula (forgot to add the *12)

--- src/main/java/com/example/week2lab/HelloController.java
+++ src/main/java/com/example/week2lab/HelloController.java
@@ -10,7 +10,7 @@ import static java.lang.Double.parseDouble;
 import static java.lang.Integer.parseInt;
 
 public class HelloController {
-
+    //initialization of working parts of the application, the @FXML tag allows the program to inherit the actual text information from the user interface.
     @FXML
     private TextField AnnualRateField;
     @FXML
@@ -25,7 +25,7 @@ public class HelloController {
     private Button calculateButton;
     @FXML
     private Label idiotLabel;
-
+    //on button press, read in text from all 3 text fields and use them in the formula. Afterward, give the values to the monthly and total payment fields.
     @FXML
     protected void calculatePayments(ActionEvent event){
     String rate = AnnualRateField.getText();
@@ -39,16 +39,18 @@ public class HelloController {
 
         MonthlyField.setText(monthly);
         TotalField.setText(yearly);
+        //I tried to use the numbers you used in the example and got something different, I guess I used the wrong formula
         idiotLabel.setText("I don't know the loan formula : v)");
     }
-
+    //called when doing the calculation for each month's payment.
     protected double calculateMonthlyPayments(double rate, double loan){
         double monthlySum = -1.0;
         monthlySum = (double) (rate * loan) /12;
         return monthlySum;
     }
+    //called when doing the total payment by multiplying the monthly payment by the yearly amount (and 12 months)
     protected double calculateTotalPayment(double rate, double years, double loan){
         double monthly = calculateMonthlyPayments(rate, loan);
-        return monthly * years;
+        return monthly * 12 * years;
     }
 }
